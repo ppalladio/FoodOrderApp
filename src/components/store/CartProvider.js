0bc02731f7a08b435b59512743fbcc3723d15707
ppalladio <nine.slide@gmail.com>
2022-10-06 21:44:23 +0200

    add items to the cart (useReducer)

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,12 +1,33 @@
 import CartContext from './cart-context';
+import { useReducer } from 'react';
+
+const defaultState = {
+    items: [],
+    totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+    if(action.type ==='ADD'){
+        const updatedItems = state.items.concat(action.item)
+        const updatedTotalAcmount = state.totalAmount + action.item.amount * action.item.price
+    return{
+        items: updatedItems,
+        totalAmount: updatedTotalAcmount,
+    }}
+    return defaultState;
+};
+
 
 const CartProvider = (props) => {
-    const addItemToCartHandler = (item) => {};
+    const [cartState, dispatchCart] = useReducer(cartReducer, defaultState)
+
+    const addItemToCartHandler = (item) => {
+        dispatchCart({type:'ADD',item:item});
+
     const removeItemFromCartHandler = (item) => {};
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     };
@@ -15,6 +36,6 @@ const CartProvider = (props) => {
             {props.children}
         </CartContext.Provider>
     );
-};
+}};
 
 export default CartProvider;
