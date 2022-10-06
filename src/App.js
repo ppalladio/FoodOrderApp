2af9259b70f6b5311c5ef1df09566796d059bb7b
ppalladio <nine.slide@gmail.com>
2022-10-06 21:05:52 +0200

    create context and contextProvider components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
-
+import CartProvider from './components/store/CartProvider';
 function App() {
     const [cartIsShown, setCartIsShown] = useState(false)
 
@@ -14,13 +14,13 @@ function App() {
         setCartIsShown(false)
     }
     return (
-        <>
+        <CartProvider>
             {cartIsShown&& <Cart onClickClose = {hideCartHandler}/>}
             <Header onCartClick={showCartHandler} />
             <main>
                 <Meals />
             </main>
-        </>
+        </CartProvider>
     );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React ,{useContext}from 'react';
 
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../store/cart-context';
 const HeaderCartButton = (props) => {
+
+    const ctx = useContext(second)
     return (
+
         <button className={classes.button} onClick = {props.onCartClick}>
             {/* //'here the name(onClick) can't be cutomized because ⬆️its not a cutomized component */}
             <span className={classes.icon}>
