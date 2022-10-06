10ef6b0f852154369e545bb5c158ef0c87c61327
Yuxuan <nine.slide@gmail.com>
2022-10-06 13:52:19 +0200

    add UI and Meal components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import Header from './components/Layout/Header';
-
-
-
+import Meals from './components/Meals/Meals';
 
 function App() {
-  return (
-    <>
-      <Header/>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <Meals />
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import classes from './Header.module.css';
-
+import HeaderCartButton from './HeaderCartButton';
 import meals from '../../Assets/meals.jpg';
 function Header(props) {
     return (
         <>
             <header className={classes.header}>
                 <h1> ReactMeal</h1>
-                <button> Cart</button>
+                <HeaderCartButton/>
             </header>
             <div className={classes['main-image']}>
                 {/* ⬆️//.can't use dot notation because there is a dash in the class name */}
